85db2338be20bf0f993620582101893a0ef676ea
Gunter-R <2004vvr@gmail.com>
2024-12-19 19:26:40 +0300

    test: add New test

--- pkg/resample_test.go
+++ pkg/resample_test.go
@@ -1,1 +1,20 @@
 package pkg
+
+import (
+	"github.com/stretchr/testify/assert"
+	"io"
+	"testing"
+)
+
+func TestResampler(t *testing.T) {
+	ir := 8000.0
+	or := 16000.0
+	ch := 1
+	frmt := I16
+	quality := -1
+	output := io.Discard
+
+	_, err := New(output, ir, or, ch, frmt, quality)
+	assert.NoError(t, err)
+
+}
