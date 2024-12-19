b35c0b48d1044b6a2167cb6062df83054a5c440c
Gunter-R <2004vvr@gmail.com>
2024-12-19 20:02:56 +0300

    test!: wirte test for a resampling func

--- resample_test.go
+++ resample_test.go
@@ -2,23 +2,16 @@ package resample
 
 import (
 	"github.com/stretchr/testify/assert"
-	"io"
 	"testing"
 )
 
 func TestResampler(t *testing.T) {
-	ir := 8000.0
-	or := 16000.0
+	input := []int16{1, 2, 3}
+	ir := 8000
+	or := 16000
 	ch := 1
-	frmt := I16
 	quality := Linear
-	output := io.Discard
 
-	res, err := New(output, ir, or, ch, frmt, quality)
+	_, err := ResampleInt16(input, ir, or, ch, quality)
 	assert.NoError(t, err)
-
-	data := []int16{1}
-	_, err = res.Write(data)
-	assert.Error(t, err)
-
 }
