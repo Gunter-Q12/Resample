b8a2882402a32310d36786712dd01c1997743362
Gunter-R <2004vvr@gmail.com>
2024-12-19 20:06:26 +0300

    feat: implement func stub

--- resample.go
+++ resample.go
@@ -1,37 +1,11 @@
 package resample
 
-import (
-	"io"
-)
-
 type Quality int
 
 const (
 	Linear Quality = iota // Linear interpolation
 )
 
-type Format int
-
-const (
-	I16 Format = iota // 16-bit signed linear PCM
-)
-
-type Resampler struct {
-	inRate      float64   // input sample rate
-	outRate     float64   // output sample rate
-	channels    int       // number of input channels
-	format      Format    // input format
-	quality     Quality   // resampling quality
-	destination io.Writer // output destination
-}
-
-func New(destination io.Writer, ir, or float64, ch int, frmt Format, q Quality) (*Resampler, error) {
-	return &Resampler{
-		inRate:      ir,
-		outRate:     or,
-		channels:    ch,
-		format:      frmt,
-		quality:     q,
-		destination: destination,
-	}, nil
+func ResampleInt16(input []int16, ir, or, ch int, quality Quality) ([]int16, error) {
+	return input, nil
 }
