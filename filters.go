1c085d08770b764366171013b01d82bca2f125b1
Gunter-R <2004vvr@gmail.com>
2025-02-06 09:24:03 +0300

    refactor(filters): move hanning-generating functions into a separate file

--- filters.go
+++ filters.go
@@ -4,7 +4,6 @@ import (
 	"embed"
 	"encoding/binary"
 	"fmt"
-	"math"
 )
 
 type Option[T Number] func(*Resampler[T]) error
@@ -114,39 +113,4 @@ func readWindowFromFile(path string, length int) ([]float64, error) {
 	}
 
 	return interpWin, nil
-}
-
-func newHanningWindow(zeros, density int) []float64 {
-	n := density * zeros
-	sincWin := make([]float64, n+1)
-	step := float64(zeros) / float64(n+1)
-	for i := 0; i < n+1; i++ {
-		sincWin[i] = sinc(float64(i) * step)
-	}
-
-	taper := hanning(2*n + 1)[n:]
-	return elementwiseProd(taper, sincWin)
-}
-
-func sinc(x float64) float64 {
-	if x == 0 {
-		return 1
-	}
-	return math.Sin(math.Pi*x) / (math.Pi * x)
-}
-
-func hanning(m int) []float64 {
-	win := make([]float64, m)
-	for n := 0; n < m; n++ {
-		win[n] = 0.5 - 0.5*math.Cos((2*math.Pi*float64(n))/float64(m-1))
-	}
-	return win
-}
-
-func elementwiseProd(a, b []float64) []float64 {
-	res := make([]float64, len(a))
-	for i := 0; i < len(a); i++ {
-		res[i] = a[i] * b[i]
-	}
-	return res
 }
