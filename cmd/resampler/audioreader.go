4f6c531b71d9fb92eede16346bbf96e1a1a4c0d4
Gunter-R <2004vvr@gmail.com>
2025-02-06 10:04:11 +0300

    fix(main): fix wav header

--- cmd/resampler/audioreader.go
+++ cmd/resampler/audioreader.go
@@ -11,6 +11,8 @@ import (
 type waveFile struct {
 	file   *os.File
 	header []byte
+
+	outRate int
 }
 
 func (wf *waveFile) Write(p []byte) (n int, err error) {
@@ -26,11 +28,25 @@ func (wf *waveFile) Close() error {
 	}
 	size := info.Size()
 
+	// FileSize
 	wf.file.Seek(4, io.SeekStart)
 	binary.Write(wf.file, binary.LittleEndian, int32(size-8))
 
+	// Frequency
+	wf.file.Seek(24, io.SeekStart)
+	binary.Write(wf.file, binary.LittleEndian, int32(wf.outRate))
+
+	// DataSize
 	wf.file.Seek(40, io.SeekStart)
 	binary.Write(wf.file, binary.LittleEndian, int32(size-44))
+
+	var bytePerBlock int16
+	wf.file.Seek(32, io.SeekStart)
+	binary.Read(wf.file, binary.LittleEndian, &bytePerBlock)
+
+	// BytePerSec
+	wf.file.Seek(28, io.SeekStart)
+	binary.Write(wf.file, binary.LittleEndian, int32(bytePerBlock)*int32(wf.outRate))
 
 	return nil
 }
@@ -45,23 +61,26 @@ func getInOutFiles(inPath, outPath string, outRate int) (io.ReadCloser, io.Write
 		return nil, nil, err
 	}
 
+	header := make([]byte, 44)
 	if strings.ToLower(filepath.Ext(inPath)) == ".wav" {
-		header := make([]byte, 44)
 		_, err = io.ReadFull(in, header)
 		if err != nil {
 			return nil, nil, err
 		}
-
+	}
+	if strings.ToLower(filepath.Ext(outPath)) == ".wav" {
 		_, err := out.Write(header)
 		if err != nil {
 			return nil, nil, err
 		}
 
-		out.Seek(24, io.SeekStart)
-		binary.Write(out, binary.LittleEndian, int32(outRate))
 		out.Seek(44, io.SeekStart)
 
-		return in, &waveFile{file: out, header: header}, nil
+		return in, &waveFile{
+			file:    out,
+			header:  header,
+			outRate: outRate,
+		}, nil
 	}
 
 	return in, out, nil
