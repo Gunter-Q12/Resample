a96d8b5c2dbe1ed980e6a7ee886a04f90480419f
Gunter-R <2004vvr@gmail.com>
2025-04-20 08:47:03 +0300

    test: refactor precision tests and add samples

--- precision_test.go
+++ precision_test.go
@@ -8,47 +8,174 @@ import (
 	"testing"
 )
 
-func TestResamplerPrecision(t *testing.T) {
-	music48Data, err := os.Open("./testdata/speech_sample_mono44.1kHz16bit.raw")
+type filterCase struct {
+	name string
+	f    resample.Option
+}
+
+func TestResamplerPrecisionSpeech(t *testing.T) {
+	speech44Data, err := os.Open("./testdata/speech_sample_mono44.1kHz16bit.raw")
+	require.NoError(t, err)
+	speech44 := unBuffer[int16](t, speech44Data)
+
+	speech14Data, err := os.Open("./testdata/speech_sample_mono14.7kHz16bit.raw")
+	require.NoError(t, err)
+	speech14 := unBuffer[int16](t, speech14Data)
+
+	downInput := testCase[int16]{
+		name: "downsampling", format: resample.FormatInt16,
+		input:  speech44,
+		output: speech14,
+		err:    nil, ir: 44100, or: 14700, ch: 1,
+	}
+
+	upInput := testCase[int16]{
+		name: "upsampling", format: resample.FormatInt16,
+		input:  speech14,
+		output: speech44,
+		err:    nil, ir: 14700, or: 44100, ch: 1,
+	}
+
+	// precision values were acquired from experiments with Resampy library
+	// that implements the same interpolation method
+	testCases := []struct {
+		nameSuffix string
+		tc         testCase[int16]
+		filter     resample.Option
+		precision  float64
+	}{
+		{
+			nameSuffix: "fastest",
+			tc:         upInput,
+			filter:     resample.WithKaiserFastestFilter(),
+			precision:  16,
+		},
+		{
+			nameSuffix: "fast",
+			tc:         upInput,
+			filter:     resample.WithKaiserFastFilter(),
+			precision:  23,
+		},
+		{
+			nameSuffix: "best",
+			tc:         upInput,
+			filter:     resample.WithKaiserBestFilter(),
+			precision:  14,
+		},
+		{
+			nameSuffix: "fastest",
+			tc:         downInput,
+			filter:     resample.WithKaiserFastestFilter(),
+			precision:  18,
+		},
+		{
+			nameSuffix: "fast",
+			tc:         downInput,
+			filter:     resample.WithKaiserFastFilter(),
+			precision:  24,
+		},
+		{
+			nameSuffix: "best",
+			tc:         downInput,
+			filter:     resample.WithKaiserBestFilter(),
+			precision:  16,
+		},
+	}
+
+	for _, tc := range testCases {
+		check(
+			t, "memoization "+tc.nameSuffix,
+			tc.tc, avgDelta[int16](tc.precision), tc.filter,
+		)
+	}
+	for _, tc := range testCases {
+		check(
+			t, "no memoization "+tc.nameSuffix,
+			tc.tc, avgDelta[int16](tc.precision), tc.filter,
+		)
+	}
+}
+
+// precision values were acquired from experiments with Resampy library
+// that implements the same interpolation method
+func TestResamplerPrecisionMusic(t *testing.T) {
+	music48Data, err := os.Open("./testdata/music_sample_mono48kHz16bit.raw")
 	require.NoError(t, err)
 	music48 := unBuffer[int16](t, music48Data)
 
-	music16Data, err := os.Open("./testdata/speech_sample_mono14.7kHz16bit.raw")
+	music16Data, err := os.Open("./testdata/music_sample_mono16kHz16bit.raw")
 	require.NoError(t, err)
 	music16 := unBuffer[int16](t, music16Data)
 
-	testCases := []testCase[int16]{
-		{name: "upsampling", format: resample.FormatInt16,
-			input:  music16,
-			output: music48,
-			err:    nil, ir: 16000, or: 48000, ch: 1},
-		{name: "downsampling", format: resample.FormatInt16,
-			input:  music48,
-			output: music16,
-			err:    nil, ir: 48000, or: 16000, ch: 1},
+	downInput := testCase[int16]{
+		name: "downsampling", format: resample.FormatInt16,
+		input:  music48,
+		output: music16,
+		err:    nil, ir: 48000, or: 16000, ch: 1,
 	}
 
-	filters := []struct {
-		name string
-		f    resample.Option
+	upInput := testCase[int16]{
+		name: "upsampling", format: resample.FormatInt16,
+		input:  music16,
+		output: music48,
+		err:    nil, ir: 16000, or: 48000, ch: 1,
+	}
+
+	testCases := []struct {
+		nameSuffix string
+		tc         testCase[int16]
+		filter     resample.Option
+		precision  float64
 	}{
-		{"fastest", resample.WithKaiserFastestFilter()},
-		{"fast", resample.WithKaiserFastFilter()},
-		{"best", resample.WithKaiserBestFilter()},
+		{
+			nameSuffix: "fastest",
+			tc:         upInput,
+			filter:     resample.WithKaiserFastestFilter(),
+			precision:  119,
+		},
+		{
+			nameSuffix: "fast",
+			tc:         upInput,
+			filter:     resample.WithKaiserFastFilter(),
+			precision:  173,
+		},
+		{
+			nameSuffix: "best",
+			tc:         upInput,
+			filter:     resample.WithKaiserBestFilter(),
+			precision:  97,
+		},
+		{
+			nameSuffix: "fastest",
+			tc:         downInput,
+			filter:     resample.WithKaiserFastestFilter(),
+			precision:  127,
+		},
+		{
+			nameSuffix: "fast",
+			tc:         downInput,
+			filter:     resample.WithKaiserFastFilter(),
+			precision:  179,
+		},
+		{
+			nameSuffix: "best",
+			tc:         downInput,
+			filter:     resample.WithKaiserBestFilter(),
+			precision:  107,
+		},
 	}
 
 	for _, tc := range testCases {
-		for _, f := range filters {
-			check(t, "memoization "+f.name, tc, avgDelta[int16](90), f.f)
-		}
+		check(
+			t, "memoization "+tc.nameSuffix,
+			tc.tc, avgDelta[int16](tc.precision), tc.filter,
+		)
 	}
 	for _, tc := range testCases {
-		for _, f := range filters {
-			check(
-				t, "no memoization "+f.name, tc, avgDelta[int16](90), f.f,
-				resample.WithNoMemoization(),
-			)
-		}
+		check(
+			t, "no memoization "+tc.nameSuffix,
+			tc.tc, avgDelta[int16](tc.precision), tc.filter,
+		)
 	}
 }
 
@@ -59,14 +186,12 @@ func avgDelta[T number](delta float64) checker[T] {
 
 		var actualDelta float64
 		for i := range expected[:commonLen] {
-			delta := actual[i] - expected[i]
-			if delta < 0 {
-				delta = -delta
-			}
-			actualDelta += float64(delta)
+			d := max(actual[i], expected[i]) - min(actual[i], expected[i])
+			actualDelta += float64(d)
 		}
 		actualDelta /= float64(commonLen)
 
+		t.Logf("average delta %.2f", actualDelta)
 		assert.LessOrEqual(t, actualDelta, delta)
 	}
 }
---
+++ testdata/README.md
@@ -0,0 +1,5 @@
+Samples were borrowed from [this repository](https://github.com/voxserv/audio_quality_testing_samples) and [this site](https://helpguide.sony.net/high-res/sample1/v1/en/index.html). Then downsampled using SOXR library with the highest quality option, then upsampled. Resulting files are used to test the accuracy of the current library.
+
+Scripts used for sine generation and other tests can be found in [this](https://gitlab.com/Gunter-Q12/resample_tests) repository.
+
+Benchmark samples are just some random sounds, input data does not matter in this case.
