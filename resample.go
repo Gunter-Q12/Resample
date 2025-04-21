8df932e7e2f0442bd8c6067b95740dae1aa1795e
Gunter-R <2004vvr@gmail.com>
2025-04-21 22:46:14 +0300

    refactor: add convolver struct

--- resample.go
+++ resample.go
@@ -47,8 +47,6 @@ type Resampler struct {
 	memoization bool
 	f           *filter
 	elemSize    int
-
-	processed int
 }
 
 // New creates a new Resampler.
@@ -88,124 +86,138 @@ func New(outBuffer io.Writer, format Format, inRate, outRate, ch int,
 }
 
 func (r *Resampler) Write(input []byte) (int, error) {
-	r.processed = 0
-	return r.write(input, 0, len(input))
-}
-
-func (r *Resampler) ReadFrom(reader io.Reader) (int64, error) {
-	r.processed = 0
-
-	wingSize := r.f.Length(0) * r.elemSize
-	middleSize := (runtime.NumCPU()*1024 + r.inRate - 1) / r.inRate * r.inRate
-	buffSize := wingSize*3 + middleSize*r.elemSize
-
-	buffer := make([]byte, buffSize)
-	read := 0
-
-	n, err := reader.Read(buffer[:middleSize+wingSize])
-	read += n
-	if err != nil && err != io.EOF {
-		return int64(read), err
-	}
-	if n < middleSize+wingSize {
-		_, err = r.write(buffer[:n], 0, n)
-		return int64(read), err
-	}
-
-	// Special case for the first part
-	_, err = r.write(buffer[:middleSize], 0, middleSize)
-	if err != nil {
-		return int64(read), err
-	}
-	_ = copy(buffer[:wingSize*2], buffer[middleSize-wingSize:middleSize+wingSize])
-
-	for {
-		n, err = reader.Read(buffer[wingSize*2 : wingSize*2+middleSize])
-		read += n
-		if err != nil && err != io.EOF {
-			return int64(read), err
-		}
-		if n < middleSize {
-			_, err = r.write(buffer[0:wingSize*2+n], wingSize, wingSize*2+n)
-			return int64(read), err
-		}
-
-		_, err = r.write(buffer[0:wingSize*2+middleSize], wingSize, wingSize+middleSize)
-		if err != nil {
-			return int64(read), err
-		}
-		_ = copy(buffer[:wingSize*2], buffer[middleSize:middleSize+wingSize*2])
-	}
-}
-
-func (r *Resampler) write(input []byte, start, end int) (int, error) {
 	switch r.format {
 	case FormatInt16:
-		return write[int16](r, input, start, end)
+		return write[int16](r, input)
 	case FormatInt32:
-		return write[int32](r, input, start, end)
+		return write[int32](r, input)
 	case FormatInt64:
-		return write[int64](r, input, start, end)
+		return write[int64](r, input)
 	case FormatFloat32:
-		return write[float32](r, input, start, end)
+		return write[float32](r, input)
 	case FormatFloat64:
-		return write[float64](r, input, start, end)
+		return write[float64](r, input)
 	default:
 		panic("unknown format")
 	}
 }
 
-type convolutionInfo[T number] struct {
-	f             *filter
-	frameFunc     frameCalcFunc[T]
-	timeIncrement float64
-	ch            int
-
-	startSample      int
-	endSample        int
-	processedSamples int
-	samples          []float64
-	output           []T
+func (r *Resampler) ReadFrom(reader io.Reader) (int64, error) {
+	switch r.format {
+	case FormatInt16:
+		return readFrom[int16](r, reader)
+	case FormatInt32:
+		return readFrom[int32](r, reader)
+	case FormatInt64:
+		return readFrom[int64](r, reader)
+	case FormatFloat32:
+		return readFrom[float32](r, reader)
+	case FormatFloat64:
+		return readFrom[float64](r, reader)
+	default:
+		panic("unknown format")
+	}
 }
 
-// write is an actual implementation of Write.
-func write[T number](r *Resampler, input []byte, start, end int) (int, error) {
-	samples, err := getSamples[T](input, r.elemSize)
+func readFrom[T number](r *Resampler, reader io.Reader) (int64, error) {
+	wingSize := r.f.Length(0) * r.elemSize
+	middleSize := (runtime.NumCPU()*1024 + r.inRate - 1) / r.inRate * r.inRate
+	buffSize := wingSize*3 + middleSize*r.elemSize
+
+	buff := make([]byte, buffSize)
+
+	c := convolver[T]{
+		r: r,
+	}
+
+	read := 0
+
+	n, err := reader.Read(buff[:middleSize+wingSize])
+	read += n
+	if err != nil && err != io.EOF {
+		return int64(read), err
+	}
+	if n < middleSize+wingSize {
+		_, err = c.resample(buff[:n], 0, n)
+		return int64(read), err
+	}
+
+	// Special case for the first part
+	_, err = c.resample(buff[:middleSize], 0, middleSize)
 	if err != nil {
-		return 0, fmt.Errorf("resampler: write: %w", err)
+		return int64(read), err
 	}
+	_ = copy(buff[:wingSize*2], buff[middleSize-wingSize:middleSize+wingSize])
 
-	startSample := start / r.elemSize
-	endSample := end / r.elemSize
+	for {
+		n, err = reader.Read(buff[wingSize*2 : wingSize*2+middleSize])
+		read += n
+		if err != nil && err != io.EOF {
+			return int64(read), err
+		}
+		if n < middleSize {
+			_, err = c.resample(buff[0:wingSize*2+n], wingSize, wingSize*2+n)
+			return int64(read), err
+		}
 
-	inFrames := (endSample - startSample) / r.ch
-	outFrames := int(float64(inFrames*r.outRate) / float64(r.inRate))
-	outSamples := outFrames * r.ch
-
-	info := convolutionInfo[T]{
-		f:             r.f,
-		frameFunc:     calcFrame[T],
-		timeIncrement: float64(r.inRate) / float64(r.outRate),
-		ch:            r.ch,
-
-		startSample:      startSample,
-		endSample:        endSample,
-		processedSamples: r.processed,
-		samples:          samples,
-		output:           make([]T, outSamples),
+		_, err = c.resample(buff[0:wingSize*2+middleSize], wingSize, wingSize+middleSize)
+		if err != nil {
+			return int64(read), err
+		}
+		_ = copy(buff[:wingSize*2], buff[middleSize:middleSize+wingSize*2])
 	}
-	if r.memoization {
-		info.frameFunc = calcFrameWithMemoization
+}
+
+func write[T number](r *Resampler, input []byte) (int, error) {
+	c := convolver[T]{
+		r: r,
 	}
+	return c.resample(input, 0, len(input))
+}
 
-	convolve[T](info)
+type convolver[T number] struct {
+	r             *Resampler
+	frameFunc     frameCalcFunc[T]
+	timeIncrement float64
 
-	err = binary.Write(r.outBuf, binary.LittleEndian, info.output)
+	startSample int
+	endSample   int
+	processed   int
+	samples     []float64
+	output      []T
+}
+
+// resample is an actual implementation of Write.
+func (c *convolver[T]) resample(input []byte, start, end int) (int, error) {
+	samples, err := getSamples[T](input, c.r.elemSize)
 	if err != nil {
-		return 0, fmt.Errorf("resampler: write: %w", err)
+		return 0, fmt.Errorf("resampler: resample: %w", err)
 	}
 
-	r.processed += outSamples
+	c.frameFunc = calcFrame[T]
+	c.timeIncrement = float64(c.r.inRate) / float64(c.r.outRate)
+	c.startSample = start / c.r.elemSize
+	c.endSample = end / c.r.elemSize
+	c.samples = samples
+
+	inFrames := (c.endSample - c.startSample) / c.r.ch
+	outFrames := int(float64(inFrames*c.r.outRate) / float64(c.r.inRate))
+	outSamples := outFrames * c.r.ch
+
+	c.output = make([]T, outSamples)
+
+	if c.r.memoization {
+		c.frameFunc = calcFrameWithMemoization
+	}
+
+	c.convolve()
+
+	err = binary.Write(c.r.outBuf, binary.LittleEndian, c.output)
+	if err != nil {
+		return 0, fmt.Errorf("resampler: resample: %w", err)
+	}
+
+	c.processed += outSamples
 	return len(input), nil
 }
 
@@ -224,16 +236,16 @@ func getSamples[T number](input []byte, elemSize int) ([]float64, error) {
 	return fSamples, nil
 }
 
-func convolve[T number](info convolutionInfo[T]) {
+func (c *convolver[T]) convolve() {
 	routines := runtime.NumCPU() * routinesPerCore
-	frames := len(info.output) / info.ch
+	frames := len(c.output) / c.r.ch
 	framesPerRoutine := (frames + routines - 1) / routines
 	if frames < routines {
 		routines = 1
 		framesPerRoutine = frames
 	}
 
-	allNewSamples := make([]float64, routines*info.ch)
+	allNewSamples := make([]float64, routines*c.r.ch)
 
 	wg := sync.WaitGroup{}
 	for i := range routines {
@@ -242,16 +254,16 @@ func convolve[T number](info convolutionInfo[T]) {
 			defer wg.Done()
 			startFrame := framesPerRoutine * i
 			batchSize := min(framesPerRoutine, frames-startFrame)
-			newSamples := allNewSamples[i*info.ch : (i+1)*info.ch]
+			newSamples := allNewSamples[i*c.r.ch : (i+1)*c.r.ch]
 			for currFrame := range batchSize {
 				outputFrame := startFrame + currFrame
-				inputTime := float64(outputFrame) * info.timeIncrement
+				inputTime := float64(outputFrame) * c.timeIncrement
 
-				info.frameFunc(info.f, info.samples, newSamples, inputTime, outputFrame, info)
+				c.frameFunc(c.r.f, c.samples, newSamples, inputTime, outputFrame, c)
 
-				startSample := outputFrame * info.ch
+				startSample := outputFrame * c.r.ch
 				for s, sample := range newSamples {
-					info.output[startSample+s] = T(sample)
+					c.output[startSample+s] = T(sample)
 					newSamples[s] = 0
 				}
 			}
@@ -260,14 +272,14 @@ func convolve[T number](info convolutionInfo[T]) {
 	wg.Wait()
 }
 
-type frameCalcFunc[T number] func(*filter, []float64, []float64, float64, int, convolutionInfo[T])
+type frameCalcFunc[T number] func(*filter, []float64, []float64, float64, int, *convolver[T])
 
-func calcFrame[T number](f *filter, samples, newSamples []float64, inputTime float64, _ int, info convolutionInfo[T]) {
+func calcFrame[T number](f *filter, samples, newSamples []float64, inputTime float64, _ int, c *convolver[T]) {
 	ch := len(newSamples)
 	batchNum := len(samples) / ch
 
-	inputFrame := int(inputTime) + (info.startSample / info.ch)
-	offset := inputTime + float64(info.processedSamples/info.ch)*info.timeIncrement
+	inputFrame := int(inputTime) + (c.startSample / c.r.ch)
+	offset := inputTime + float64(c.processed/c.r.ch)*c.timeIncrement
 	offset = offset - float64(int(offset))
 
 	// computing left wing including the middle element
@@ -294,14 +306,14 @@ func calcFrame[T number](f *filter, samples, newSamples []float64, inputTime flo
 }
 
 func calcFrameWithMemoization[T number](
-	f *filter, samples, newSamples []float64, inputTime float64, outputFrame int, info convolutionInfo[T],
+	f *filter, samples, newSamples []float64, inputTime float64, outputFrame int, c *convolver[T],
 ) {
 	ch := len(newSamples)
 	batchNum := len(samples) / ch
 
 	offsetsNum := len(f.offsetWins)
-	inputFrame := int(inputTime) + (info.startSample / info.ch)
-	offset := (outputFrame + info.processedSamples) % offsetsNum
+	inputFrame := int(inputTime) + (c.startSample / c.r.ch)
+	offset := (outputFrame + c.processed) % offsetsNum
 
 	// computing left wing including the middle element
 	iters := min(len(f.offsetWins[offset]), inputFrame+1)
